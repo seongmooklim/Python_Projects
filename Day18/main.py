ca22f0ea59a80114071cb97e64e99b8977993fd9
LIM <seongmook.lim@gmail.com>
2024-06-21 12:37:04 +0900

    [Day 18] Draw shapes like triangle, square, pentagon...decagon.
    and random walk
    and draw spiograpth

--- Day18/main.py
+++ Day18/main.py
@@ -1,15 +1,28 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 import random
 
 tim = Turtle()
-colours = ['red', 'yellow', 'violet', 'green', 'blue', 'black']
-direction = [0,90,180,270]
-tim.pensize(10)
-tim.speed(10)
-for _ in range(200):
-    tim.color(random.choice(colours))
-    tim.forward(30)
-    tim.setheading(random.choice(direction))
+tim.speed(100)
+def draw_spriograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spriograph(5)
+
+screen = Screen()
+screen.exitonclick()
+
+
+# colours = ['red', 'yellow', 'violet', 'green', 'blue', 'black']
+# direction = [0,90,180,270]
+# tim.pensize(10)
+# tim.speed(10)
+# for _ in range(200):
+#     tim.color(random.choice(colours))
+#     tim.forward(30)
+#     tim.setheading(random.choice(direction))
+
 # def draw_shape(num_sides):
 #     angle = 360 / num_sides
 #     for i in range(num_sides):
