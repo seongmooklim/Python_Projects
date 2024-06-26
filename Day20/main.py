dc725c7e5cd96f615e393591fd0488b0b208b313
LIM <seongmook.lim@gmail.com>
2024-06-26 14:52:42 +0900

    [Day 20] Snake game.
    1. Create Snake
    2. Move Snake

--- Day20/main.py
+++ Day20/main.py
@@ -1,6 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
 screen = Screen()
 screen.setup(600,600)
 screen.bgcolor('black')
@@ -10,6 +13,14 @@ screen.tracer(0)
 segments = []
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 # Move Snake
@@ -17,5 +28,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
