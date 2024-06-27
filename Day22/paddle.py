980fa5abdea5cc3dfccfd3b03074dfb3c1ce97a9
Lim <seongmook.lim@gmail.com>
2024-06-27 23:47:53 +0900

    Day19 Turtle race

--- Day22/paddle.py
+++ Day22/paddle.py
@@ -1,11 +1,13 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
+
     def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid=6, stretch_len=1)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
 
