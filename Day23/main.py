7539d77b48317e06ff00f90018e54db873dc338f
Lim <seongmook.lim@gmail.com>
2024-06-28 00:56:27 +0900

    Day23 Turtle game

--- Day23/main.py
+++ Day23/main.py
@@ -7,8 +7,14 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
+
+screen.listen()
+screen.onkey(player.up,"Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- Day23/player.py
+++ Day23/player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.heading()
+        self.goto(STARTING_POSITION)
+        self.shape('turtle')
+        self.color('black')
 
-class Player:
-    pass
+    def up(self):
+        new_y = self.ycor() + 10
+        self.goto(self.xcor(), new_y)
