0859d97e2e290070598a54f103706e37f50ac843
LIM <seongmook.lim@gmail.com>
2024-07-02 17:10:49 +0900

    [Day 24] Mail merge project

--- Day24_Mail_Merge_Prj/main.py
+++ Day24_Mail_Merge_Prj/main.py
@@ -15,8 +15,9 @@ with open("./input/Names/invited_names.txt") as names_file:
     
 with open("./input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
-    for name in names:
-        stripped_name = name.strip()
-        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt",mode='w') as completed_letter:
-            completed_letter.write(new_letter)
+
+for name in names:
+    stripped_name = name.strip()
+    new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+    with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt",mode='w') as completed_letter:
+        completed_letter.write(new_letter)
