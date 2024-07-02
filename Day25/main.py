386817c0c0d9588cc9669ccd35a5f9c882670062
Lim <seongmook.lim@gmail.com>
2024-07-03 00:38:29 +0900

    Day25 Pandas squirrel count

--- Day25/main.py
+++ Day25/main.py
@@ -3,5 +3,15 @@
 #     print(data)
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color" : ["Gray", "Cinnamon", "Black"],
+    "Count" : [gray_count, red_count, black_count ]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
