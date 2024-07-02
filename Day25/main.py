cef077cc3f5e41d19e466c7d5f93fb51c449dd4e
LIM <seongmook.lim@gmail.com>
2024-07-02 17:54:42 +0900

    [Day 24] Practice CSV File

--- Day25/main.py
+++ Day25/main.py
@@ -4,4 +4,28 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+
+# temp_list = data['temp'].to_list()
+# mean = sum(temp_list)/len(temp_list)
+# print(mean)
+# print(data['temp'].max())
+# print(data['condition'])
+#
+# max_temp = data['temp'].max()
+# print(data[data.temp == max_temp])
+
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp
+f_temp = monday_temp*9/5+32
+print(f"f_temp is {f_temp}")
+
+#Create a dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "Scores": [76,12,66]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_csv_file.csv")
+
