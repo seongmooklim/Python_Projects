d9eb184f562a87dc3bb157a19fe99cca57c6f4d3
LIM <seongmook.lim@gmail.com>
2024-07-03 16:53:25 +0900

    [Day 25] States Game - export csv file

--- Day25_Find States in the North America/main.py
+++ Day25_Find States in the North America/main.py
@@ -16,7 +16,14 @@ guessed_state = []
 for i in range(50):
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 Guess State Correct",
                                     prompt="What's another state's name").title()
-    print(answer_state)
+    if answer_state == 'Exit':
+        missing_state = []
+        for state in all_state:
+            if state not in guessed_state:
+                missing_state.append(state)
+        df = pandas.DataFrame(missing_state)
+        df.to_csv("missing_state.csv")
+        break
 
     if answer_state in all_state:
         guessed_state.append(answer_state)
