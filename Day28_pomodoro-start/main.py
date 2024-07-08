bbaed9b2341e748418e7d0f785063fbfcdf6461a
LIM <seongmook.lim@gmail.com>
2024-07-08 17:28:28 +0900

    [Day 28] restructure itmes

--- Day28_pomodoro-start/main.py
+++ Day28_pomodoro-start/main.py
@@ -20,12 +20,30 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
+
 canvas.create_image(100,112, image=tomato_img)
 canvas.create_text(100,130,text="00:00",fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
-)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+title_label.grid(column=1, row=0)
+
+
+def start_button():
+    pass
+def Reset_button():
+    pass
+
+start_button = Button(text='Start', highlightthickness=0, command=start_button)
+start_button.grid(column=0, row=2)
+
+Reset_button = Button(text='Reset', highlightthickness=0, command=start_button)
+Reset_button.grid(column=2, row=2)
+
+check_label = Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+check_label.grid(column=1, row=3)
 
 window.mainloop()
