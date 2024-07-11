0076c8ba9285ac1046ac53f921c5b3ab52766b42
LIM <seongmook.lim@gmail.com>
2024-07-11 18:03:46 +0900

    [Day 31] Capstone Project - Flash Cards

--- Day31_Capstone_Project/main.py
+++ Day31_Capstone_Project/main.py
@@ -1,2 +1,23 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=900, height=676)
+card_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(450,290, image=card_img)
+canvas.config(bg=BACKGROUND_COLOR)
+canvas.grid(column=0, row=0)
+
+right_image = PhotoImage(file="images/right.png")
+right_image_label = Label(window, image=right_image)
+right_image_label.grid(column=0, row=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_image_label = Label(window, image=wrong_image)
+wrong_image_label.grid(column=2, row=2)
+
+window.mainloop()
