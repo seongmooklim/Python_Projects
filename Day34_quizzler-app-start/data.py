c49968ef8202a3c0b411cfcf412df206710cb74f
Lim <seongmook.lim@gmail.com>
2024-07-14 01:48:48 +0900

    [Day34] Quizzler-qpp : Modify data.py

--- Day34_quizzler-app-start/data.py
+++ Day34_quizzler-app-start/data.py
@@ -9,7 +9,7 @@ response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
-print(question_data)
+# print(question_data)
 
 # question_data = [
 #     {
