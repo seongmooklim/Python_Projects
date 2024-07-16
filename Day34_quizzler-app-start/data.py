556bf4932b04e32716778ff5e48f56ba4d1e1540
LIM <seongmook.lim@gmail.com>
2024-07-16 16:23:08 +0900

    [Day 34] Quizzler modify category

--- Day34_quizzler-app-start/data.py
+++ Day34_quizzler-app-start/data.py
@@ -3,7 +3,8 @@ import requests
 
 parameters = {
     'amount' : 10,
-    'type' : 'boolean'
+    'type' : 'boolean',
+    'category' : 18
 }
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
