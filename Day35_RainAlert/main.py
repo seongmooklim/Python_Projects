421cbe16d9a21c9ed505d8bf6432b0a28fd2b7c3
Lim <seongmook.lim@gmail.com>
2024-07-17 22:46:35 +0900

    [Day35] RainAlert

--- Day35_RainAlert/main.py
+++ Day35_RainAlert/main.py
@@ -1,1 +1,18 @@
-sample
+import requests
+
+parameters = {
+    'lat' : 37.440559,
+    'lon' : 127.13583,
+    'appid' : '252dffee55c4b3c731a578a363feb106'
+}
+response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["main"]
+
+print(question_data)
+
+#
+# https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=37.440559&lon=127.135834&appid=252dffee55c4b3c731a578a363feb106
+#
+# https://api.openweathermap.org/data/2.5/weather?lat=37.440559&lon=127.13583&appid=252dffee55c4b3c731a578a363feb106
