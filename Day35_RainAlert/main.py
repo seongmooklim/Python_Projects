6cb401a9a90b1a9acdfec97de4a8324da75116ff
Lim <seongmook.lim@gmail.com>
2024-07-17 23:22:50 +0900

    [Day35] RainAlert

--- Day35_RainAlert/main.py
+++ Day35_RainAlert/main.py
@@ -1,16 +1,25 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.8/onecall"
+api_key = "252dffee55c4b3c731a578a363feb106"
 parameters = {
-    'lat' : 37.440559,
-    'lon' : 127.13583,
-    'appid' : '252dffee55c4b3c731a578a363feb106'
+    'lat': 37.440559,
+    'lon': 127.13583,
+    'appid': api_key,
+    'exclude': 'current,minutely,daily '
 }
-response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response = requests.get(url=OWM_Endpoint, params=parameters)
 response.raise_for_status()
 data = response.json()
-question_data = data["main"]
+# data_slice = data['hourly'][:12]
+# print(data_slice)
+for i in range(7,19):
+    id = data['hourly'][i]['weather'][0]['id']
+    if(id<700):
+        will_rain = True
 
-print(question_data)
+if will_rain:
+    print("Bring an Umbrella")
 
 #
 # https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=37.440559&lon=127.135834&appid=252dffee55c4b3c731a578a363feb106
