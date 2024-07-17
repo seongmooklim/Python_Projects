911ed9e575981bf5e849bc93f11bbcc7e39a484b
LIM <seongmook.lim@gmail.com>
2024-07-17 16:03:27 +0900

    [Day32] Birthday-wisher-hard-start

--- Day32_Birthday/main.py
+++ Day32_Birthday/main.py
@@ -13,8 +13,10 @@ today_tuple = (today.month, today.day)
 
 data = pd.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+print(birthdays_dict)
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
+    #print(birthday_person)
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
 
     with open(file_path) as letter_file:
